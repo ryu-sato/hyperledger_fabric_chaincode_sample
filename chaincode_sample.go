7d9af9edbd7945be679f9ba1257af7ae82cf9036
Ryu Sato <ryu@weseek.co.jp>
2020-12-01 18:36:13 +0000

    init

---
+++ chaincode_sample.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+var logger = shim.NewLogger("chaincode_sample")
+
+type SmartContract struct {
+}
+
+// chaincode の初期化
+func (t *SmartContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	logger.Info("Chaincode initialized.")
+	return shim.Success(nil) // 何もしない
+}
+
+// トランザクションの実行
+// args[0]: トランザクション名
+// args[1..*]: コマンドに渡す引数
+func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	cmdMap := map[string]interface{}{
+		"helloChaincode": HelloChaincode,
+	}
+	cmdName, cmdArgs := stub.GetFunctionAndParameters()
+
+	if cmdInterface, exist := cmdMap[cmdName]; exist {
+		cmd := reflect.ValueOf(cmdInterface)
+		out := cmd.Call([]reflect.Value{reflect.ValueOf(stub), reflect.ValueOf(cmdArgs)})
+		if ret, ok := out[0].Interface().(peer.Response); ok {
+			return ret
+		}
+		return shim.Error(fmt.Sprintf("Invoke: Wrong response type (given %T, expected peer.Response)", out[0].Interface()))
+	}
+
+	keys := reflect.ValueOf(cmdMap).MapKeys()
+	return shim.Error(fmt.Sprintf("Invoke: Invalid command (given %s, expexted %v", cmdName, keys))
+}
+
+func HelloChaincode(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	bytes, _ := json.Marshal("hello chaincode!");
+	return shim.Success(bytes);
+}
+
+func main() {
+	logLevel := shim.LogInfo
+	if os.Getenv("SHIM_LOGGING_LEVEL") != "" {
+		logLevel, _ = shim.LogLevel(os.Getenv("SHIM_LOGGING_LEVEL"))
+	}
+	logger.SetLevel(logLevel)
+	shim.SetLoggingLevel(logLevel)
+
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		logger.Errorf("Error hello chaincode: %s\n", err)
+	}
+}
+
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/ryu-sato/chaincode_sample
+
+go 1.14
+
+require (
+	github.com/fsouza/go-dockerclient v1.6.6 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
+	github.com/hyperledger/fabric v1.4.7
+	github.com/hyperledger/fabric-amcl v0.0.0-20200424173818-327c9e2cf77a // indirect
+	github.com/miekg/pkcs11 v1.0.3 // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/sykesm/zap-logfmt v0.0.4 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	google.golang.org/grpc v1.33.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
